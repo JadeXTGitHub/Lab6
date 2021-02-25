b7f1e2fbaec0e725b899052da775b82813f98fb7
JadeXTGitHub <78270947+JadeXTGitHub@users.noreply.github.com>
2021-02-25 16:16:27 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Lab 6</title>
+<meta charset="utf-8">
+<style>
+body { background-color: #000066; }
+
+#wrapper { width: 80%;
+	        margin-left: auto;
+	     	margin-right: auto;
+		    background-color: #EAEAEA; }
+
+header { background-color: #CCCCFF; }
+
+h1 { margin: 0;
+     padding: 10px; }
+
+nav { float: left;
+      width: 90px;
+      padding: 10px; }
+
+nav ul { list-style-type: none; }
+
+
+main { margin-left: 100px;
+       padding: 10px;
+       background-color: #FFFFFF; 
+       display: block, }
+
+footer { text-align: center;
+         font-style: italic;
+         background-color: #CCCCFF;
+         clear: both; }
+</style>
+</head>
+<body>
+<div id="wrapper">
+  <header><h1>Layout Example - LAB 6</h1></header>
+  <nav>
+      <ul>
+    <li><a href="index.html">Home</a></li>
+    <li><a href="page1.html">Page 1</a></li>
+    <li><a href="page2.html">Page 2</a></li>
+</ul>
+  </nav>
+  <main>
+    <h2>Page Heading</h2>
+    <p>This is a sentence. This is a sentence. This is a sentence. This is a sentence. This is a sentence. This is a sentence. This is a sentence. This is a sentence. This is a sentence. This is a sentence. This is a sentence. This is a sentence. This is a sentence. This is a sentence. This is a sentence. I took a stab at verticle navigation with an unordered list but I left the link underlines.</p>
+  </main>
+  <footer>Copyright &copy; 2021 <br>
+      Jennifer Thomas - STIM 3320
+  </footer>
+</div>
+</body>
+</html>
+
+
+
